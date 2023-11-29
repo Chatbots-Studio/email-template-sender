8d9476eebbbc0814a21aaf3d7cf263777bcb7d4f
AgentosUA <oleh.stepaniuk@chatbots.studio>
2023-11-30 00:58:59 +0200

    feat: send multiple without targeting

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 **Just simply:**
 1. yarn
 2. create .env file and fill by example in .env.example
-3. run with "yarn run start" 
+3. Put your html templates in "templates" folder
+4. run script with "yarn run start" 
 
 You will need to get your google app code. <br>
 Visit: https://stackoverflow.com/a/72477193
--- index.js
+++ index.js
@@ -4,68 +4,109 @@ const fs = require('fs');
 const nodemailer = require('nodemailer');
 const path = require('path');
 
-if (
-  !process.env.user ||
-  !process.env.pass ||
-  !process.env.to ||
-  !process.env.templates
-) {
+if (!process.env.user || !process.env.pass || !process.env.to) {
   console.log(
-    'Please provide user, pass & to in .env file. Check .env.example for reference'
+    '\x1b[31m',
+    'Please provide "user", "pass" and "to" in .env file.\nCheck .env.example for reference\n',
+    '\x1b[0m'
   );
 
-  return process.exit(1);
+  return process.exit(0);
 }
 
-const sendEmail = (template) => {
-  if (template.includes('.html')) {
-    template = template.replace('.html', '');
+const skipeedTemplates = [];
+const sendedTemplates = [];
+
+const sendEmail = async (template) =>
+  new Promise((resolve) => {
+    fs.readFile(
+      path.join(__dirname, `./templates/${template}.html`),
+      'utf-8',
+      (err, data) => {
+        if (err) {
+          skipeedTemplates.push(`${template}.html`);
+
+          console.log(
+            '\x1b[33m',
+            `Can't find "${template}.html" in "templates" folder. Skipping...`,
+            '\x1b[0m'
+          );
+
+          return resolve();
+        }
+
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: process.env.user,
+            pass: process.env.pass,
+          },
+        });
+
+        const mailOptions = {
+          from: process.env.user,
+          to: process.env.to,
+          subject: `Your Email Template ${template}.html`,
+          html: data,
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+            return;
+          }
+
+          sendedTemplates.push(`${template}.html`);
+          resolve(template);
+        });
+      }
+    );
+  });
+
+const start = async () => {
+  const promises = [];
+
+  const directoryPath = path.join(__dirname, 'templates');
+  const templates = process.env.templates.length
+    ? process.env.templates?.split(',')?.map((template) => template.trim()) ??
+      []
+    : [];
+
+  if (!templates || !templates?.length) {
+    await new Promise((resolve) =>
+      fs.readdir(directoryPath, (err, files) => {
+        if (err) {
+          console.log('Unable to scan templates directory: ' + err);
+          resolve();
+        }
+
+        files.forEach(function (file) {
+          if (file.includes('.htm')) {
+            templates.push(file);
+          }
+        });
+
+        resolve();
+      })
+    );
   }
 
-  fs.readFile(
-    path.join(__dirname, `./templates/${template}.html`),
-    'utf-8',
-    (err, data) => {
-      if (err) {
-        console.log(
-          `Posibly you forget to put your template in "templates/${template}.html or set name in .env"\n`
-        );
-
-        return process.exit(1);
-      }
-
-      const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: process.env.user,
-          pass: process.env.pass,
-        },
-      });
-
-      const mailOptions = {
-        from: process.env.user,
-        to: process.env.to,
-        subject: 'Sending Email Template using Node.js',
-        html: data,
-      };
-
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(
-            `Email template "${template}.html" sent: ` + info.response
-          );
-        }
-      });
+  for (let template of templates) {
+    if (template.includes('.html')) {
+      template = template.replace('.html', '');
     }
-  );
+
+    promises.push(sendEmail(template));
+  }
+
+  await Promise.all(promises);
+
+  console.table({
+    '\x1b[32mSended templates:\x1b[0m': sendedTemplates,
+    '\x1b[33mSkipped templates:\x1b[0m': skipeedTemplates,
+  });
+
+  process.exit(0);
 };
 
-const templates = process.env.templates
-  .split(',')
-  .map((template) => template.trim());
-
-for (let i = 0; i < templates.length; i++) {
-  sendEmail(templates[i]);
-}
+start();
